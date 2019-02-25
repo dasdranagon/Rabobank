0341c4874fecc78ba017d6b607460878ef793012
Dennis Skokov <dennis@splendo.com>
2019-02-25 10:00:49 +0100

    interactor output tests

--- Rabobank/Screens/List/Tests/ListInteractorOutputTests.swift
+++ Rabobank/Screens/List/Tests/ListInteractorOutputTests.swift
@@ -13,5 +13,57 @@ import Quick
 
 class ListInteractorOutputTests: QuickSpec {
     override func spec() {
+        var presenter: ListPresenter!
+        var view: ListViewMock!
+        
+        beforeEach {
+            view = ListViewMock()
+            
+            presenter = ListPresenter()
+            presenter.view = view
+        }
+        
+        describe("update") {
+            context("common") {
+                beforeEach {
+                    presenter.update(items: [])
+                }
+                
+                it("stops progressing") {
+                    expect(view.processingIndicatorWasShowed) == false
+                }
+                
+                it("updates display items") {
+                    expect(view.updatedItems).notTo(beNil())
+                }
+            }
+            
+            context("non empty list") {
+                let person = Person(firstName: "Jon",
+                           surName: "Doe",
+                           issueCount: 3,
+                           dateOfBirth: Date(timeIntervalSince1970: 541241571)) // 25.02.1987
+                beforeEach {
+                    presenter.update(items: [person])
+                }
+                
+                it("presents non empty list") {
+                    expect(view.updatedItems?.count) > 0
+                }
+                
+                it("makes correct full name") {
+                    expect(view.updatedItems?.first?.fullName) == "Jon Doe"
+                }
+                
+                it("makes correct counter") {
+                    expect(view.updatedItems?.first?.fullName) == "3"
+                }
+                
+                it("makes correct date") {
+                    expect(view.updatedItems?.first?.dateOfBirth) == "15.02.1987"
+                }
+            }
+            
+        }
     }
 }
